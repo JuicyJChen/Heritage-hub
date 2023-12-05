31cb0dd1ee3c57965ccb3c27f9be8ca17f1f193f
nivers23 <nivers@umich.edu>
2023-12-05 14:15:40 -0500

    languagefunc

--- frontend/src/components/Navbar/Navbarwrapper.jsx
+++ frontend/src/components/Navbar/Navbarwrapper.jsx
@@ -28,6 +28,18 @@ const Navbarwrapper = () => {
                   Sign up
                 </Link>
               </div>
+              <div className={style.dropdown}>
+                  <label htmlFor="language">Language:</label>
+                  <select id="language" name="language">
+                    <option value="english">English</option>
+                    <option value="español">Español</option>
+                    <option value="português">Português</option>
+                    <option value="中文">中文</option>
+                    <option value="हिन्दी">हिन्दी</option>
+
+
+                  </select>
+                </div>
             </div>
           </div>
         </div>
