e8fa39ec291bcad3db88d45ba7ae40c17f83c639
nivers23 <nivers@umich.edu>
2023-12-05 14:02:42 -0500

    changedlogoevents

--- frontend/src/components/Pages/EventCreation/CommonTop.jsx
+++ frontend/src/components/Pages/EventCreation/CommonTop.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import pics from "./pic1.png"
 import { Link, useHistory } from "react-router-dom"
 const CommonTop = () => {
 
@@ -14,7 +15,7 @@ const CommonTop = () => {
         <div>
             <div>
                 <div style={{margin:"10px auto", width:"fit-content"}}>
-                    <img style={{ width: "11%", padding:"12px 16px"}} src="https://www.meetup.com/mu_static/en-US/logo--script.004ada05.svg" alt="meet-up-logo" onClick={ handleRedirect}/>
+                    <img style={{ width: "11%", padding:"12px 16px"}} src={pics} alt="meet-up-logo" onClick={ handleRedirect}/>
                     <Link to="/"><button  style={{ float: "right", padding: "28px 20px", backgroundColor: "white", border: "0", cursor: "pointer" }} >Save and Exit</button></Link>
                 </div>
                 
